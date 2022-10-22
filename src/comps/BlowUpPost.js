3feb313e6f6a94f8763faad3d41f6bd60af77ac9
ddawg776 <ddawg776@gmail.com>
2022-10-22 00:53:09 -0500

    Added working system to msg people

--- src/comps/BlowUpPost.js
+++ src/comps/BlowUpPost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useOutletContext, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -15,8 +15,33 @@ const BigPost = () => {
     // const outParams = useParams().id;
     // console.log('this is params', useParams())
     console.log('other params', id)
+
     
     const specificPost = bigData[id];
+    const [message, setMessage] = useState();
+   async function MessageUser (event) {
+    event.preventDefault();
+        try {
+        
+ 
+            const fetchReq =  await fetch(`https://strangers-things.herokuapp.com/api/2209-ftb-mt-web-ft/posts/${specificPost._id}/messages`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({
+                    message: {
+                        content: message
+                    }
+                })
+            })
+            const fetchedData = await fetchReq.json();
+            console.log('fetched data', fetchedData)
+          }  catch (error) {
+        console.log(error)
+         }}
+    MessageUser();
     console.log('this is post', specificPost);
     return (
         <div>
@@ -30,13 +55,16 @@ const BigPost = () => {
             <h6 className="p">Post Created at: {specificPost.createdAt}</h6>
             <h5 className="p">Located at {specificPost.location}</h5>
              </div>
-             <form>
+             <form onSubmit={MessageUser}>
                 <label> Msg User</label>
-                <input type='text'></input>
+                <input type='text' onChange={(event) => {
+                    console.log(event.target.value)
+                    setMessage(event.target.value)
+                }}></input>
                 <input type='submit'></input>
              </form>
         </div>
     )
 }
 
-export default BigPost;
+export default BigPost
